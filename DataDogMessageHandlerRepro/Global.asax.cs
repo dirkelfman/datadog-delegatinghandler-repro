f21b6be276ce34ffa416fcc396329d3ad2dfe03f
Sparkle Phipps <dirkelfman@gmail.com>
2018-11-07 11:18:01 -0600

    first

--- DataDogMessageHandlerRepro/Global.asax.cs
+++ DataDogMessageHandlerRepro/Global.asax.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
+using System.Web.Http.ExceptionHandling;
 using System.Web.Routing;
 
 namespace DataDogMessageHandlerRepro
@@ -12,6 +13,8 @@ namespace DataDogMessageHandlerRepro
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+            GlobalConfiguration.Configuration.MessageHandlers.Add(new MyDelegatingHandler());
+            GlobalConfiguration.Configuration.Services.Replace(typeof(IExceptionHandler), new MyExceptionHandler());
         }
     }
 }
---
+++ DataDogMessageHandlerRepro/MyDelegatingHandler.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http.ExceptionHandling;
+
+namespace DataDogMessageHandlerRepro
+{
+    public class MyDelegatingHandler : DelegatingHandler
+    {
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            HttpResponseMessage retVal = null;
+
+            try
+            {
+                retVal = await base.SendAsync(request, cancellationToken);
+            }
+            //should hit this branch
+            catch (MyException)
+            {
+                retVal = request.CreateResponse(HttpStatusCode.OK, new string[] { "correct Exception was thrown" });
+            }
+            //should not hit this branch.
+            catch (AggregateException)
+            {
+                retVal = request.CreateResponse(HttpStatusCode.OK, new string[] { "with datadog trace running an AggregateException is thrown" });
+            }
+            return retVal;
+        }
+    }
+    public class MyExceptionHandler : ExceptionHandler
+    {
+        public override bool ShouldHandle(ExceptionHandlerContext context)
+        {
+            return false;
+        }
+    }
+    public class MyException : Exception
+    {
+
+
+    }
+}
